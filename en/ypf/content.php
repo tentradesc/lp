04d836c9f47bc49d75dc5d7ead22de0e90acfc0c
Andy Cholakov <andy@acdigital.agency>
2025-06-02 02:58:26 +0300

    English initial commit

---
+++ en/ypf/content.php
@@ -0,0 +1,340 @@
+<header class="header p-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6 col-6">
+                <a href="#"><img src="assets/logo.png" alt="" class="logo-img"></a>
+            </div>
+            <div class="col-lg-6 col-md-6 col-6 text-end">
+                <div class="dropdown langauge-box">
+                    <a class="dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        EN
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown" style="">
+                        <li><a class="dropdown-item" href="../Tentrade-YPF-LP-ES/"> ES</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="head-bg">
+    <section class="hero-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-7 col-md-12">
+                    <h2 class="fw-bold text-capitalize mb-3 hero-title">Argentina’s Energy Future <br
+                            class="d-none d-lg-block"> Starts Here
+                    </h2>
+                    <p class="mb-3 hero-content">Discover YPF — A National Leader with Global Potential</p>
+                    <p class="mb-4 hero-content1">YPF is Argentina’s flagship energy company, leading the way in <br>
+                        oil and gas innovation, infrastructure, and transformation. </p>
+
+                </div>
+                <div class="col-lg-5 col-md-12 text-center">
+                    <div class="form-box">
+                        <h2 class="form-title text-center mb-4">Get Started</h2>
+                        <form class="main-form" class="main-form" id="formsubmit" method="POST" >
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6">
+                                    <input type="text" id="firstname" name="firstname" class="form-control"
+                                        placeholder="First Name" required="">
+                                </div>
+                                <div class="col-lg-6  col-md-6">
+                                    <input type="text" id="lastname" name="lastname" class="form-control"
+                                        placeholder="Last Name" required="">
+                                </div>
+                                <div class="col-lg-6  col-md-6">
+                                    <input type="tel" id="phone-1" name="phone" class="form-control" placeholder="+54"
+                                        required="">
+                                </div>
+                                <div class="col-lg-6  col-md-6">
+                                    <input type="email" id="email" name="email" class="form-control" placeholder="Email"
+                                        required="">
+                                </div>
+                                <input type="hidden" id="utm_source" name="utm_source"
+                                    value="<?php echo htmlspecialchars($utm_source) ?>" />
+                                <input type="hidden" id="utm_campaign" name="utm_campaign"
+                                    value="<?php echo htmlspecialchars($utm_campaign) ?>" />
+                                <input type="hidden" id="utm_medium" name="utm_medium"
+                                    value="<?php echo htmlspecialchars($utm_medium) ?>" />
+                                <input type="hidden" id="utm_content" name="utm_content"
+                                    value="<?php echo htmlspecialchars($utm_content) ?>" />
+                                <input type="hidden" id="utm_term" name="utm_term"
+                                    value="<?php echo htmlspecialchars($utm_term) ?>" />
+                                <input type="hidden" id="lead" name="lead" class="form-control1" value="false" />
+                                <input type="hidden" id="lang" name="lang" class="form-control1" value="it" />
+                                <input type="hidden" id="country" name="country" class="form-control1" value="" />
+                                <input type="hidden" id="city" name="city" class="form-control1" value="" />
+                                <div class="col-lg-12 text-center mb-4">
+                                    <button type="submit" class="submit-btn main-btn-form">Learn More About YPF’s Growth
+                                        Strategy<i class="fa fa-chevron-right ms-3" aria-hidden="true"></i></button>
+                                </div>
+                            </div>
+                            <div class="checkbox-wrapper d-flex justify-content-center">
+                                <label>
+                                    By registering, you agree to our
+                                    <a href="https://cabinet.10tradefx.com/uploads/public/company-documents/2025/02/10/c7db949f13949e59025ad01c61076749.pdf?utm_source=investing&utm_medium=lp&utm_campaign=investing-lp"
+                                        target="_blank">Terms and Conditions</a>
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="why-choose-section">
+        <div class="container">
+            <h2 class="main-title fw-bold text-center">About YPF </h2>
+            <p class="main-content text-center">For nearly a century, YPF (Yacimientos Petrolíferos Fiscales) has played
+                a vital role in Argentina’s energy growth. As a state-backed company with vast experience across the
+                energy chain, YPF is helping power the future — from traditional resources to advanced energy
+                solutions. </p>
+
+        </div>
+    </section>
+</div>
+<section class="how-to-invest">
+    <div class="container">
+        <h2 class="section-title text-center fw-bold">Why It Matters Now</h2>
+        <div class="row number-box-space">
+            <div class="col-lg-3 col-md-3 border-r d-flex">
+                <div class="number-box">
+                    <h2 class="text-center">1</h2>
+                    <div>
+                        <h3 class="fw-bold text-center"> Strategic Ownership</h3>
+                        <p class="text-center mb-0"> Backed by the Argentine government for long-term national
+                            relevance </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 border-r d-flex">
+                <div class="number-box">
+                    <h2 class="text-center">2</h2>
+                    <h3 class="fw-bold text-center">Vaca Muerta Advantage</h3>
+                    <p class="text-center mb-0">Operates the largest share in one of the world’s richest shale reserves 
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 border-r d-flex">
+                <div class="number-box">
+                    <h2 class="text-center">3</h2>
+                    <h3 class="fw-bold text-center">Energy Demand Growth</h3>
+                    <p class="text-center mb-0">Positioned to support rising demand across Latin America and beyond </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 d-flex">
+                <div class="number-box">
+                    <h2 class="text-center">4</h2>
+                    <h3 class="fw-bold text-center">Ongoing Modernization</h3>
+                    <p class="text-center mb-0">Investing in refining, infrastructure, and efficiency </p>
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+            <a href="#" class="btn main-btn">Access the Full Company Report  <i class="fa fa-chevron-right ms-3"
+                    aria-hidden="true"></i></a>
+        </div>
+    </div>
+</section>
+
+<section class="image-section">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <h2 class="text-white fw-bold mb-3 section-title text-center">A Moment of Opportunity </h2>
+                <p class="text-white section-content  text-center">With infrastructure, reserves, and international
+                    interest aligning, Argentina’s energy sector is undergoing a transformative shift — and YPF is at
+                    its center. Explore why industry experts are watching this story unfold. </p>
+            </div>
+
+        </div>
+
+        <div class="text-center pt-5">
+            <a href="#" class="btn main-btn2">Discover Argentina’s Energy Investment Landscape  <i
+                    class="fa fa-chevron-right ms-3" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</section>
+
+
+<section>
+    <div class="container">
+        <h2 class="text-center fw-bold mb-3 tradingview-title">
+            Why YPF Oil
+        </h2>
+        <p class="text-center tradingview-text">YPF is strategically positioned to benefit from the global energy
+            shift—and now, with oil prices at historic lows, the window of opportunity for smart investors has never
+            been wider.</p>
+        <!-- TradingView Widget BEGIN -->
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container" style="">
+            <div class="tradingview-widget-container__widget" style=""></div>
+            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow"
+                    target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+            <script type="text/javascript"
+                src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+                    {
+                        "width": "100%",
+                            "height": "650",
+                                "symbol": "NYSE:YPF",
+                                    "interval": "D",
+                                        "timezone": "Etc/UTC",
+                                            "theme": "dark",
+                                                "style": "1",
+                                                    "locale": "en",
+                                                        "backgroundColor": "rgba(0, 0, 0, 1)",
+                                                            "hide_top_toolbar": true,
+                                                                "allow_symbol_change": true,
+                                                                    "hide_volume": true,
+                                                                        "support_host": "https://www.tradingview.com"
+                    }
+                </script>
+        </div>
+        <!-- TradingView Widget END -->
+        <!-- TradingView Widget END -->
+        <div class="text-center pt-4">
+            <a href="#" class="btn main-btn">Become a YPF Investor <i class="fa fa-chevron-right ms-3"
+                    aria-hidden="true"></i></a>
+        </div>
+    </div>
+</section>
+<section class="gold-bg">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 order-2 order-lg-1">
+                <p class="img-box-text">YPF offers a rare blend of tangible assets, government backing, global
+                    relevance, and massive untapped potential. As energy dynamics shift worldwide, positioning yourself
+                    in a company sitting on world-class reserves with growth initiatives underway is more than a smart
+                    move—it’s a strategic one.</p>
+                <div class="text-left py-3 mobile-box">
+                    <a href="#" class="btn main-btn">Register Now <i class="fa fa-chevron-right ms-3"
+                            aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex order-1 order-lg-2">
+                <div class="image-bg">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="container">
+
+        <div class="row align-items-center pb-5">
+            <div class="col-lg-6 col-md-6">
+                <a href="#"> <img src="assets/footer-logo.png" class="footer-logo" width="100%"></a>
+            </div>
+            <div class="col-lg-6 col-md-6">
+            </div>
+        </div>
+
+        <p>
+            RISK DISCLAIMER “Contract for Differences” (CFDs) are usually leveraged products. Trading Over-The-Counter
+            (OTC) CFDs related to commodities, Forex, Indices and Shares, carries a high level of risk and can result in
+            the loss of all of your investment. As such, CFDs may not be appropriate and/ or suitable for all investors.
+            You should not invest money that you cannot afford to lose. Before deciding to trade, you should be aware of
+            all the risks associated with OTC CFDs trading, and seek advice from an independent and suitably licensed
+            financial advisor. Past performance does not constitute a reliable indicator of future results. Future
+            forecasts do not constitute a reliable indicator of future performance. General information and/ or
+            recommendations provided by the Company should not be interpreted as investment advice. For more information
+            please visit our General Risk Disclosure Policy.
+        </p>
+        <p>
+            Disclaimer: Information on this site is not directed at residents in any country or jurisdiction where such
+            distribution or use would be contrary to local law or regulation
+        </p>
+
+    </div>
+</footer>
+
+
+<script>
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('.header');
+        if (window.scrollY > 0) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+</script>
+<style>
+    .error-border {
+        border: 1px solid red !important;
+    }
+
+    label.error {
+        display: none !important;
+    }
+</style>
+<!-- Form JS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.16/css/intlTelInput.css"
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.16/js/intlTelInput.min.js"
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.16/js/utils.js" crossorigin="anonymous"
+    referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+
+        var telInput = $("#phone-1");
+
+        // Initialize intl-tel-input with Argentina as the default
+        telInput.intlTelInput({
+            separateDialCode: true,
+            allowDropdown: true,
+            initialCountry: "ar", // Set default to Argentina
+            geoIpLookup: function (callback) {
+                callback("ar"); // Force Argentina regardless of IP
+            }
+        });
+        // Function to setup the form
+        function setupForm(formId, phoneId, countryId, isoId) {
+            const form = $(`#${formId}`);
+            const telInput = $(`#${phoneId}`);
+
+            // For normal phone input without country flags
+            telInput.on("input", function () {
+                $(this).removeClass("error-border");  // Remove error border on user input
+            });
+
+            form.validate({
+                rules: {
+                    firstname: "required",
+                    lastname: "required",
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    phone: "required",
+                    terms: "required"
+                },
+                highlight: function (element) {
+                    $(element).addClass("error-border");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass("error-border");
+                },
+                errorPlacement: function () {
+                    return false;
+                },
+                submitHandler: function () {
+                    form[0].submit(); // Submit the form to thankyou.php
+                }
+            });
+        }
+
+        // Call setup for both forms
+        setupForm("main-form-1", "phone-1", "country-1", "iso-1");
+
+    });
+</script>
