0f154da915c4212b96a797c9cb7ad326b9481562
Andy Cholakov <andy@acdigital.agency>
2025-06-02 13:40:34 +0300

    jQuery import modification

--- body.php
+++ body.php
@@ -1,7 +1,7 @@
 <body>
     <?php include __DIR__ . '/gtm_body.php'; ?>
-    <script src="https://lp.tentrade.com/includes/js/user-location.js"></script>
-    <script src="https://lp.tentrade.com/includes/js/form-submit.js"></script>
+    <script src="https://lp.ten.trade/includes/js/user-location.js"></script>
+    <script src="https://lp.ten.trade/includes/js/form-submit.js"></script>
     <?php include $pageContent; ?>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 </body>
